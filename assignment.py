fbccb02d5424fef5b663a090761c5a9d5e0941b1
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-04-10 15:23:18 -0500

    first problem

---
+++ assignment.py
@@ -0,0 +1,23 @@
+# Problem 1
+
+''' Sets Adventure
+Objective:
+ The aim of this assignment is to deepen your understanding and applicaiton of Python sets in various scenarios, 
+ ranging from basic operations to advanced manipulations and best practices.
+ You will correct given codes, use sets in everyday contexts, and tackle challenges that require creative thinking and problem-solving.
+
+Task 1:
+ Imagine you work for an airline and need to compare the flight routes of your airline with a competitor.
+ You have two sets of flight destinations, one for each airline. 
+ Write a Python program to find out:
+
+ 1) Destinations that both airlines fly to.
+ 2) Destinations unique to your airline.
+ 3) Whether there are any destinations that neither airline shares.
+
+Example Code:
+
+our_routes = {"LAX", "JFK", "CDG", "DXB"}
+competitor_routes = {"JFK", "CDG", "LHR", "BKK"}
+
+'''
